fe935123ee48204457d3484ebcfd9c3856916ef5
Jin Gong <gongjin21@hotmail.com>
2023-03-31 22:36:11 +0800

    Rust call console.log

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,15 @@ use wasm_bindgen::prelude::*;
 // Calling external functions in JavaScript from Rust
 #[wasm_bindgen]
 extern {
-    pub fn alert(s: &str);
+    // pub fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 // Producing Rust functions that JavaScript can call
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    // alert(&format!("Hello, {}!", name));
+    log(&format!("Hello, {}!", name));
 }
